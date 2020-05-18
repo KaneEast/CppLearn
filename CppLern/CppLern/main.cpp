be13a15029a8e4f31efee66dc5d7ababa1bdedb6
Kane.in <kin2@gw.nsw.co.jp>
2020-05-18 16:05:51 +0900

    create project

---
+++ CppLern/CppLern/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppLern
+//
+//  Created by USER on 2020/05/18.
+//  Copyright © 2020 USER. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
