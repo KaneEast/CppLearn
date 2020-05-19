304b40ac5fc9356252e9a09c343d0565645dcf2f
Kane.in <kin2@gw.nsw.co.jp>
2020-05-19 20:17:50 +0900

    commit

---
+++ CppLern/CppLern/Tests/std_algorithm.cpp
@@ -0,0 +1,9 @@
+//
+//  std_algorithm.cpp
+//  CppLern
+//
+//  Created by USER on 2020/05/19.
+//  Copyright © 2020 USER. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ CppLern/CppLern/Tests/vector_test.cpp
@@ -0,0 +1,9 @@
+//
+//  vector_test.cpp
+//  CppLern
+//
+//  Created by USER on 2020/05/19.
+//  Copyright © 2020 USER. All rights reserved.
+//
+
+#include <stdio.h>
